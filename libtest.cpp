85d5da964a092812cee43bdfd3f7fb7a0db94f88
Yuriy Yakimenko <intheswim@gmail.com>
2021-02-21 21:11:41 -0800

    added license note

--- libtest.cpp
+++ libtest.cpp
@@ -1,3 +1,15 @@
+/* LZW static library test. 
+ * Copyright (c) 2021 Yuriy Yakimenko
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation.  No representations are made about the suitability of this
+ * software for any purpose.  It is provided "as is" without express or 
+ * implied warranty.
+ */
+
 #include <cstring>
 #include <cstdlib>
 #include <cstdio>
